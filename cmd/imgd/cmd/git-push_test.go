f16153b9e17cbf5ef1210230e160ce9ef4b322d0
zinego <1083112307@qq.com>
2021-10-19 23:17:17 +0800

    feat: test.

--- cmd/imgd/cmd/git-push_test.go
+++ cmd/imgd/cmd/git-push_test.go
@@ -2,11 +2,9 @@ package cmd
 
 import (
 	"testing"
-
-	"github.com/zinego/go-tools/utils/log"
 )
 
 func Test_publicKey(t *testing.T) {
-	log.Init()
-	publicKey("")
+	cfgFile = "../testdata/.imgd.yaml"
+	initConfig()
 }
