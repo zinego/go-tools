d5231ba7d90c232fd2459b4fdab3d455e943c44a
zinego <1083112307@qq.com>
2021-11-06 17:52:48 +0800

    feat: test

--- math/dataframe/dataframe_test.go
+++ math/dataframe/dataframe_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/zinego/go-tools/utils/log"
 )
 
 func TestLoadFromMapStringFloat64(t *testing.T) {
@@ -19,4 +20,15 @@ func TestLoadFromMapStringFloat64(t *testing.T) {
 		defer f.Close()
 		assert.Nil(t, r.WriteCSV(f))
 	})
+	t.Run("test", func(t *testing.T) {
+		test(1, 2, 4)
+		test(1, 2, 342, 423, 423, 423, 4, 234, 23)
+	})
+}
+
+func test(i ...int) {
+	log.Init()
+	for _, v := range i {
+		log.Info(v)
+	}
 }
